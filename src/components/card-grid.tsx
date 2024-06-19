ed76749ec7493c1d11557348919f64ebb96baf22
Willem <91901507+wverbuyst@users.noreply.github.com>
2024-06-19 09:03:54 +0200

    Get basic navigation working

--- src/components/card-grid.tsx
+++ src/components/card-grid.tsx
@@ -23,7 +23,7 @@ function CardComponent({
       <CardHeader>
         <CardTitle className="text-3xl text-white">{name}</CardTitle>
       </CardHeader>
-      <CardContent className="flex w-full h-full items-center justify-center">
+      <CardContent className="flex items-center justify-center">
         {children}
       </CardContent>
     </Card>
@@ -31,56 +31,243 @@ function CardComponent({
 }
 
 export default function CardGrid() {
-  const [grid, setGrid] = useState({
-    col1: [
+  const [grid, setGrid] = useState<{
+    [key: number]: { name: string; color: string }[];
+  }>({
+    1: [
       { name: "foo", color: "bg-red-500" },
       { name: "bar", color: "bg-green-500" },
       { name: "baz", color: "bg-blue-500" },
     ],
-    col2: [
+    2: [
       { name: "qux", color: "bg-pink-500" },
       { name: "quux", color: "bg-teal-500" },
       { name: "corge", color: "bg-yellow-500" },
     ],
-    col3: [
+    3: [
       { name: "grault", color: "bg-purple-500" },
       { name: "garply", color: "bg-orange-500" },
       { name: "waldo", color: "bg-indigo-500" },
     ],
   });
 
+  const handleMove = ({
+    direction,
+    col,
+    index,
+    item,
+  }: {
+    direction: "up" | "down" | "left" | "right";
+    col: 1 | 2 | 3;
+    index: number;
+    item: { name: string; color: string };
+  }) => {
+    const newGrid = { 1: [...grid[1]], 2: [...grid[2]], 3: [...grid[3]] };
+
+    switch (direction) {
+      case "left":
+        if (col === 1) return;
+        // @ts-expect-error
+        newGrid[col - 1].push(item);
+        newGrid[col].splice(index, 1);
+        break;
+      case "right":
+        if (col === 3) return;
+        // @ts-expect-error
+        newGrid[col + 1].push(item);
+        newGrid[col].splice(index, 1);
+        break;
+      case "up":
+        if (index === 0) return;
+        newGrid[col].splice(index, 1);
+        newGrid[col].splice(index - 1, 0, item);
+        break;
+      case "down":
+        if (index === newGrid[col].length - 1) return;
+        newGrid[col].splice(index, 1);
+        newGrid[col].splice(index + 1, 0, item);
+        break;
+      default:
+        break;
+    }
+
+    setGrid(newGrid);
+  };
+
   return (
-    <main className="h-screen p-24 grid grid-cols-3">
-      {grid.col1.map((m) => (
-        <CardComponent key={m.name} name={m.name} color={m.color}>
-          <section className="text-white flex gap-2">
-            <MoveLeftIcon size={40} />
-            <MoveDownIcon size={40} />
-            <MoveUpIcon size={40} />
-            <MoveRightIcon size={40} />
-          </section>
-        </CardComponent>
-      ))}
-      {grid.col2.map((m) => (
-        <CardComponent key={m.name} name={m.name} color={m.color}>
-          <section className="text-white flex gap-2">
-            <MoveLeftIcon size={40} />
-            <MoveDownIcon size={40} />
-            <MoveUpIcon size={40} />
-            <MoveRightIcon size={40} />
-          </section>
-        </CardComponent>
-      ))}
-      {grid.col3.map((m) => (
-        <CardComponent key={m.name} name={m.name} color={m.color}>
-          <section className="text-white flex gap-2">
-            <MoveLeftIcon size={40} />
-            <MoveDownIcon size={40} />
-            <MoveUpIcon size={40} />
-            <MoveRightIcon size={40} />
-          </section>
-        </CardComponent>
-      ))}
+    <main className="p-24 flex justify-center gap-4">
+      <section className="flex flex-col w-[350px] gap-4">
+        {grid[1].map((m, i) => (
+          <CardComponent key={m.name} name={m.name} color={m.color}>
+            <section className="text-white flex gap-2">
+              <MoveLeftIcon
+                className="cursor-pointer"
+                size={40}
+                onClick={() =>
+                  handleMove({
+                    direction: "left",
+                    col: 1,
+                    index: i,
+                    item: m,
+                  })
+                }
+              />
+              <MoveDownIcon
+                className="cursor-pointer"
+                size={40}
+                onClick={() =>
+                  handleMove({
+                    direction: "down",
+                    col: 1,
+                    index: i,
+                    item: m,
+                  })
+                }
+              />
+
+              <MoveUpIcon
+                className="cursor-pointer"
+                size={40}
+                onClick={() =>
+                  handleMove({
+                    direction: "up",
+                    col: 1,
+                    index: i,
+                    item: m,
+                  })
+                }
+              />
+
+              <MoveRightIcon
+                className="cursor-pointer"
+                size={40}
+                onClick={() =>
+                  handleMove({
+                    direction: "right",
+                    col: 1,
+                    index: i,
+                    item: m,
+                  })
+                }
+              />
+            </section>
+          </CardComponent>
+        ))}
+      </section>
+      <section className="flex flex-col w-[350px] gap-4">
+        {grid[2].map((m, i) => (
+          <CardComponent key={m.name} name={m.name} color={m.color}>
+            <section className="text-white flex gap-2">
+              <MoveLeftIcon
+                className="cursor-pointer"
+                size={40}
+                onClick={() =>
+                  handleMove({
+                    direction: "left",
+                    col: 2,
+                    index: i,
+                    item: m,
+                  })
+                }
+              />
+              <MoveDownIcon
+                className="cursor-pointer"
+                size={40}
+                onClick={() =>
+                  handleMove({
+                    direction: "down",
+                    col: 2,
+                    index: i,
+                    item: m,
+                  })
+                }
+              />
+
+              <MoveUpIcon
+                className="cursor-pointer"
+                size={40}
+                onClick={() =>
+                  handleMove({
+                    direction: "up",
+                    col: 2,
+                    index: i,
+                    item: m,
+                  })
+                }
+              />
+
+              <MoveRightIcon
+                className="cursor-pointer"
+                size={40}
+                onClick={() =>
+                  handleMove({
+                    direction: "right",
+                    col: 2,
+                    index: i,
+                    item: m,
+                  })
+                }
+              />
+            </section>
+          </CardComponent>
+        ))}
+      </section>
+      <section className="flex flex-col w-[350px] gap-4">
+        {grid[3].map((m, i) => (
+          <CardComponent key={m.name} name={m.name} color={m.color}>
+            <section className="text-white flex gap-2">
+              <MoveLeftIcon
+                className="cursor-pointer"
+                size={40}
+                onClick={() =>
+                  handleMove({
+                    direction: "left",
+                    col: 3,
+                    index: i,
+                    item: m,
+                  })
+                }
+              />
+              <MoveDownIcon
+                className="cursor-pointer"
+                size={40}
+                onClick={() =>
+                  handleMove({
+                    direction: "down",
+                    col: 3,
+                    index: i,
+                    item: m,
+                  })
+                }
+              />
+              <MoveUpIcon
+                className="cursor-pointer"
+                size={40}
+                onClick={() =>
+                  handleMove({
+                    direction: "up",
+                    col: 3,
+                    index: i,
+                    item: m,
+                  })
+                }
+              />
+              <MoveRightIcon
+                className="cursor-pointer"
+                size={40}
+                onClick={() =>
+                  handleMove({
+                    direction: "right",
+                    col: 3,
+                    index: i,
+                    item: m,
+                  })
+                }
+              />
+            </section>
+          </CardComponent>
+        ))}
+      </section>
     </main>
   );
 }
