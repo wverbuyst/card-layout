7abb36e34670fd682ec326f1552876c81ca98e43
Willem <91901507+wverbuyst@users.noreply.github.com>
2024-06-20 05:38:43 +0200

    Update readme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
 import CardLayout from "@/components/card-layout";
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "CardLayout",
+};
 
 export default function Home() {
   return <CardLayout />;
